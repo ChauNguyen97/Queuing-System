eab9c62896abe9caf1120521adb727ada1f3d884
unknown <18424013@student.hcmus.edu.vn>
2022-10-17 10:44:37 +0700

    add header

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,7 +1,24 @@
+import Images from 'constants/images';
 import React from 'react';
+import './header.css';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <h1 className="headerTitle">Thông tin cá nhân</h1>
+      <div className="headerTool">
+        <div className="headerNotion">
+          <img src={Images.NOTION_ICON} alt="" className="headerIcon" />
+        </div>
+        <img src={Images.USER_PICTURE} alt="" className="headerImage" />
+
+        <div className="headerInfo">
+          <p className="headerText">Xin chào</p>
+          <h2 className="headerName">Lê Quỳnh Ái Vân</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/constants/images.ts
+++ src/constants/images.ts
@@ -11,11 +11,14 @@ import layer from '../assets/images/fi_layers.svg';
 import service from '../assets/images/service.svg';
 import report from '../assets/images/report.svg';
 import logout from '../assets/images/fi_log-out.svg';
+import notion from '../assets/images/notification.svg';
+import picture from '../assets/images/unsplash_Fyl8sMC2j2Q.png';
 
 const Images = {
   BANNER01: banner01,
   BANNER02: banner02,
   LOGO: logo,
+  USER_PICTURE: picture,
   WARNING_ICON: warningIcon,
   EYE_ICON: eyeSlash,
   DASHBOARD_ICON: dashboard,
@@ -26,6 +29,7 @@ const Images = {
   LAYER_ICON: layer,
   REPORT_ICON: report,
   LOGOUT_ICON: logout,
+  NOTION_ICON: notion,
 };
 
 export default Images;
